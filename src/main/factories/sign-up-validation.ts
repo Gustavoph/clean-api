c79e0074fdfc6de2efceb5a3a519de5c3be63998
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-11-04 16:19:29 -0300

    feat: add compare fields validation on SignUpController

--- src/main/factories/sign-up-validation.ts
+++ src/main/factories/sign-up-validation.ts
@@ -1,10 +1,11 @@
 import { Validation } from '@/presentation/protocols'
-import { RequiredFieldValidation, ValidationComposite } from '@/presentation/validations'
+import { RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '@/presentation/validations'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['name', 'password', 'passwordConfirmation', 'sigeCode', 'permission']) {
     validations.push(new RequiredFieldValidation(field))
   }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
--- src/presentation/validations/index.ts
+++ src/presentation/validations/index.ts
@@ -1,2 +1,3 @@
 export * from './required-fields'
 export * from './validation-composite'
+export * from './compare-fields'
